5deb0cfcd344a21d6b4663371cb6d9baeb801bc0
murilo-carazato <2311600157@fema.edu.br>
2024-06-19 09:39:24 -0300

    autoload do composer iniciado

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu de Navegação</title>
+    <style>
+
+    </style>
+</head>
+
+<body>
+    <h1>Menu de Navegação</h1>
+    <ul>
+       
+        <li>TESTE CARAZATO</li>
+
+        <li><a class="" href="/App/VIEW/INVENTARIO/lstInventario.php">lstInventario.php</a></li>
+        <li><a class="" href="/App/VIEW/INVENTARIO/formDetInventario.php?id=1">formDetInventario.php</a></li>
+        <li><a class="" href="/App/VIEW/EQUIPAMENTO/formEdtInventario.php?id=1">formEdtInventario.php</a></li>
+        <li><a class="" href="/App/VIEW/INVENTARIO/formInventario.php">formInventario.php</a></li>
+        <li><a class="" href="/App/VIEW/RECEITA/lstReceita.php">lstReceita.php</a></li>
+        <li><a class="" href="/App/VIEW/RECEITA/formReceita.php">formReceita.php</a></li>
+
+    </ul>
+</body>
+
+</html>
