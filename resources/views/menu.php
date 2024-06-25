b5304e01f59a60dd844c28b072f9c9ce7358edd0
Vantini <m.henrique.vantini18@gmail.com>
2024-06-25 15:57:04 -0300

    fix: bloquear repetição de item no carrinho

--- resources/views/menu.php
+++ resources/views/menu.php
@@ -50,9 +50,19 @@ if ($_GET['search']) {
             }
         },
         isOpen: false,
+        repeated: false,
         addToCart(product){
-            this.cart.push(product);
-            localStorage.setItem('cart',JSON.stringify(this.cart));
+            if(this.cart.length>0){
+                for(let i=0; i < this.cart.length; i++){
+                    if(product.id == this.cart[i].id){
+                        this.repeated = true;
+                    }
+                }
+            }
+            if(!this.repeated){
+                this.cart.push(product);
+                localStorage.setItem('cart',JSON.stringify(this.cart));
+            }
         },
         clearCart(){
             this.cart = [];
@@ -145,7 +155,11 @@ if ($_GET['search']) {
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                                 </svg>
                             </div>
-                            <input type="search" id="default-search" name="search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Pesquise um produto..." value="<?php  if(isset($search)) {echo$search;} else{echo"";}  ?>" />
+                            <input type="search" id="default-search" name="search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Pesquise um produto..." value="<?php if (isset($search)) {
+                                                                                                                                                                                                                                                                                                                                                                                                                    echo $search;
+                                                                                                                                                                                                                                                                                                                                                                                                                } else {
+                                                                                                                                                                                                                                                                                                                                                                                                                    echo "";
+                                                                                                                                                                                                                                                                                                                                                                                                                }  ?>" />
                             <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-blue-800">Pesquisar</button>
                         </div>
                     </form>
@@ -234,7 +248,7 @@ if ($_GET['search']) {
                                         'image': 'https:cdn.pixabay.com/photo/2016/11/19/18/06/feet-1840619_1280.jpg',
                                         'title': '<?php echo $product->getDescription() ?>',
                                         'price': '<?php echo $product->getUnitPrice() ?>'
-                                })" class="text-white bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-blue-800">Comprar</button>
+                                }); repeated=false" class="text-white bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-blue-800">Comprar</button>
                                 </div>
                             </div>
                             <?php
